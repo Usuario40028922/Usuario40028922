5f1e383626f37a86da44aeee4064351dab465527
Usuario40028922 <165251305+Usuario40028922@users.noreply.github.com>
2024-11-09 21:15:44 -0300

    Update lib.py

--- pasta/src/lib.py
+++ pasta/src/lib.py
@@ -1,4 +1,10 @@
+# 1° - Função de criar o tabuleiro e guardar em uma variável.
+# 2° - Função de mostrar o tabuleiro e outra para atualizar o tabuleiro.
+
+
 import os
+
+alfabeto_maiusculo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 def clean():
     # Verifica o sistema operacional
@@ -10,38 +16,97 @@ def clean():
 def printes(lista):
    for x in lista:
       print(x, end="\n")
-      input
-   
+      input()
 
+def seu_terminal_aguenta():
+  import os
 
-def nascer_dic(largura, altura):
-  alfabeto_maiusculo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  tamanho_terminal = os.get_terminal_size()
+
+  largura = tamanho_terminal.columns
+
+  # (3 + (8 * 2)) + (N * 3) = 140
+  # 19 + (N * 3) = 140
+  # (N * 3) = 140 - 19
+  # N = 121 / 3
+  # N = 40,3
+  # 19 + (40 * 3) = 139
+  # igual a 140 se a variavel for igual a 40,33333333333333
+
+  a_partir_de_dez_soma_tres = (largura - 19) / 3
+  formula = 3 + 16 + a_partir_de_dez_soma_tres * 3
+  print(f"Você só pode até uns {(10 + a_partir_de_dez_soma_tres) - 1}")
+
+  print(largura, formula)
+  # Tamanho em colunas (largura) e linhas (altura)
+
+def nascer_dic(largura, altura, betoalfa):
+  alfabeto_maiusculo = betoalfa
 
   dicionario = {}
 
+  alfabeto = []
+  num = 1
+
   for x in range(altura):
+    if x > len(alfabeto_maiusculo) - 1:
+      for y in range(len(alfabeto_maiusculo)):
+        alfabeto.append(f"{alfabeto_maiusculo[y]}{num}")
+      num += 1
+      alfabeto_maiusculo += alfabeto
+      print(alfabeto_maiusculo)
+
     key = alfabeto_maiusculo[x]
     dicionario[key] = "~" * largura
 
-  return dicionario
+  return dicionario, alfabeto_maiusculo
 
 
 
 
-def mostrar_dic(largura, altura):
-  alfabeto_maiusculo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  print("  1", end=" ")
-  for x in range(largura - 1):
-    print(f"{x + 2}", end=" ")
+def mostrar_dic(dicionario, betoalfa):
+  alfabeto_maiusculo = betoalfa
+  
+  num = 1
+  print(" ", end="")
+  for x in dicionario.keys():
+    print(f" {num}", end="")
+    #print(f"{num}", end=" ")
+    num += 1
   print()
 
-  v = "~ "
 
-  for y in range(altura):
-    print(f"{alfabeto_maiusculo[y]} {v * largura}")
+  valor = len(dicionario['A'])
+  v = "~"
+  nada = " "
+  
+  for z in range(len(dicionario.keys())):
+    print(f"{alfabeto_maiusculo[z]}", end="")
+
+    for y in range(valor):
+      if z >= 26 and y >= 34:
+        print(f" {v}", end=" ")
+        continue
+      elif z >= 26:
+         print(f"{v}", end=" ")
+         continue
+
+      if y >= 8 and y < 100:
+          v += nada
+
+      #elif y > 100 and y < 1000:
+          #print(f"{alfabeto_maiusculo[y]} {v}")
+          #continue
+      print(f" {v}", end="")
+      v = "~"
+
+    print()
 
 
 if __name__ == '__main__':
-  dicionario = nascer_dic(10, 10)
-  print(dicionario)
-  mostrar_dic(10, 10)
+  
+  seu_terminal_aguenta()
+  dicionario, alfabeto = nascer_dic(49, 49, alfabeto_maiusculo)
+  print(len(alfabeto))
+  print("."*139)
+  mostrar_dic(dicionario, alfabeto)
